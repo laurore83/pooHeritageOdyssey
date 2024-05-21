db9f58e965985c6830038010adc6c66fb215e8cd
Laurore83 <laurore83@yahoo.fr>
2024-05-21 19:28:54 +0200

    poo heritage odissey

---
+++ src/main/java/Hangar.java
@@ -0,0 +1,9 @@
+public class Hangar {
+    public static void main(String[] args) {
+
+        Boat yacht = new Boat("yacht", 1500);
+        Car twingo = new Car("twingo", 800);
+
+    System.out.println(yacht.doStuff());
+System.out.println(twingo.doStuff());
+}}
